9791bec18988a93eee22452b986e75a5329130f4
Maksim <maxkotov@gmail.com>
2018-04-20 13:19:55 -0400

    Form login/registration added

---
+++ js/homePageController.js
@@ -0,0 +1,8 @@
+function showMainPageView(){
+    $('#mainForm').hide();
+    $('#carouselHotelControls').show();
+}
+function showMainFormView(){
+    $('#mainForm').show();
+    $('#carouselHotelControls').hide();
+}
